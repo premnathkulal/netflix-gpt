ed3eb87e710f15690f0af8d411d4417a96f9764b
Premnathkulal <premnathkulal199@gmail.com>
2024-02-14 22:25:26 +0530

    Added required assets

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import netflixLog from '../../assets/netflix-logo.png'
 
 const Header = () => {
     return (
         <div className="header">
+            <img src={netflixLog} alt="logo" />
             Header
         </div>
     )
