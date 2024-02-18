0efbf6d6035240f933510f8ce549bd79ab0e7c87
Premnathkulal <premnathkulal199@gmail.com>
2024-02-18 16:49:16 +0530

    Upadted authentication ande adde Updapdate usee profile feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import './App.scss';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import { useEffect } from 'react';
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from './utils/firebase-config';
+import { useDispatch } from 'react-redux';
+import { addUser, removeUser } from './store/slices/user-slice';
+
 
 function App() {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName, photoURL } = user;
+        dispatch(addUser({ uid, email, displayName, photoURL }))
+        navigate('/browse')
+      } else {
+        dispatch(removeUser())
+        navigate('/auth')
+      }
+    });
+  }, [])
+
   return (
     <div className="app">
       <Header />
---
+++ src/store/slices/user-slice.ts
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState: {
+        uid: null,
+        email: null,
+        displayName: null,
+        photoURL: null,
+    },
+    reducers: {
+        addUser: (state, action) => {
+            return action.payload;
+        },
+
+        removeUser: () => {
+            return {
+                uid: null,
+                email: null,
+                displayName: null,
+                photoURL: null,
+            };
+        },
+    },
+});
+
+export const { addUser, removeUser } = userSlice.actions;
+export default userSlice.reducer;
